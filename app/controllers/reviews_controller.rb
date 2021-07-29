bf7b83b1ada56fd67eb18d56260a85c8242a6967
Nahum <losond8@gmail.com>
2021-07-29 07:08:59 -0700

    commit tree 6700b533efdcb8705e6c63c0082be6901f8d931d

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, except: [:created_at, :updated_at], include: :dog_house
   end
 
 end
